31b68cf547027f6cd4baaa9bbd4caf19242e4842
Miquel <miquelmuntanerweb@gmail.com>
2022-06-18 15:44:37 +0200

    website converted to pwa

--- js/script.js
+++ js/script.js
@@ -302,12 +302,3 @@ try {
 } catch(err) {
     console.log(err);
 }
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
